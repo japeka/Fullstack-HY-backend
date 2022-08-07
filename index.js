6cf97f61c1e2d215c22470fb65c3082cf34f6030
Janne Kalliokulju <janne.kalliokulju@gmail.com>
2022-08-07 16:30:32 +0300

    puhelinluettelo morgan support added

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
+var morgan = require('morgan')
+
 const app = express()
 app.use(express.json())
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body)
+    ].join(' ')
+  })
+)
+
 const generateId = () => Math.floor(Math.random()*1000)
 
 let persons = 
@@ -79,6 +94,11 @@ let persons =
     }
     response.json(newPerson)
   })  
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  app.use(unknownEndpoint)
 
   const PORT = 3001
   app.listen(PORT, () => {
